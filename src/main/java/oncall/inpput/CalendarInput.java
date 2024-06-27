e102563f45a7be072b08e01c4b5b0b5926bf2f90
idealflower-k <aekrcu@naver.com>
2024-06-27 17:24:55 +0900

    fix: change input process

--- src/main/java/oncall/inpput/CalendarInput.java
+++ src/main/java/oncall/inpput/CalendarInput.java
@@ -22,15 +22,27 @@ public class CalendarInput {
         return instance;
     }
 
-    public MyCalendar createCalendarFromInput() throws IllegalArgumentException {
-        String line = Console.readLine();
-        String[] split = line.split(",");
-        if (split.length != 2 || !isValidMonth(split[0]) || !isValidDay(split[1])) {
-            throw new IllegalArgumentException();
+    public MyCalendar createCalendarFromInput() {
+        boolean isValid = false;
+
+        while (!isValid) {
+            try {
+                System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+                String line = Console.readLine();
+                String[] split = line.split(",");
+                if (split.length != 2 || !isValidMonth(split[0]) || !isValidDay(split[1])) {
+                    throw new IllegalArgumentException();
+                }
+
+                MyMonth month = new MyMonth(Integer.parseInt(split[0]));
+                DayEnum dayEnum = DayEnum.getByKoreaName(split[1]);
+                isValid = true;
+                return new MyCalendar(dayEnum, month);
+            } catch (IllegalArgumentException exception) {
+                System.out.println(EnumError.ERROR_HEADER.getMessage() + " " + EnumError.ERROR_BODY.getMessage());
+            }
         }
-        MyMonth month = new MyMonth(Integer.parseInt(split[0]));
-        DayEnum dayEnum = DayEnum.getByKoreaName(split[1]);
-        return new MyCalendar(dayEnum, month);
+        return new MyCalendar(null, new MyMonth(0));
     }
 
     private boolean isValidMonth(String data) {
