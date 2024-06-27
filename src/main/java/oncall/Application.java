5848356a8daf9a87731eadaa92a8398b05653205
idealflower-k <aekrcu@naver.com>
2024-06-27 20:47:33 +0900

    feat: 애플리케이션 메인문 구현

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,24 @@
 package oncall;
 
+import camp.nextstep.edu.missionutils.Console;
+import oncall.controller.AllocateController;
+import oncall.inpput.CalendarInput;
+import oncall.inpput.EnumError;
+import oncall.inpput.WorkerInput;
+import oncall.model.calendar.MyCalendar;
+import oncall.model.worker.WorkerManger;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        CalendarInput calendarInput = CalendarInput.getInstance();
+        WorkerInput workerInput = WorkerInput.getInstance();
+
+        MyCalendar calendarFromInput = calendarInput.createCalendarFromInput();
+        WorkerManger workerManagerFromInput = workerInput.createWorkerManagerFromInput();
+
+        AllocateController controller = new AllocateController(calendarFromInput, workerManagerFromInput);
+        controller.allocateWorker();
+        controller.printResult();
     }
 }
